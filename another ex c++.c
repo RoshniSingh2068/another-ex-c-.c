1bbd0b88294793262fba34953eb0b137b9627d07
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:31:45 +0530

    Create another ex c++.c

---
+++ another ex c++.c
@@ -0,0 +1,16 @@
+// old enough to vote or not by the help else
+#include <iostream>
+using namespace std ;
+int main(){
+    int myAge = 19;
+    int votingAge = 18;
+    if (myAge>=votingAge){
+            cout << "old enough to vote ";
+
+    } else{
+        cout<<"not enough to vote get lost" ;
+
+    }
+    return 0;
+
+}
